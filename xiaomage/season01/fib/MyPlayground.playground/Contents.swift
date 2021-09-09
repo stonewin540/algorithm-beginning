5df0f0321006f9a8bec56b13cb5fb75fc43b309c
sidun.wen <sidun.wen@linkingcities.com>
2021-09-09 20:54:29 +0800

    Fibonacci sequence;

---
+++ xiaomage/season01/fib/MyPlayground.playground/Contents.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+// 斐波那契数列
+// 0, 1, 2, 3, 4, 5, 6, 7, 8
+// 0, 1, 1, 2, 3, 5, 8, 13, 21
+
+// recursively
+func fib1(_ num: Int) -> Int {
+    if num <= 1 {
+        return num
+    }
+    let res = fib1(num - 1) + fib1(num - 2)
+    return res
+}
+
+// iterator
+func fib2(_ num: Int) -> Int {
+    if num <= 1 {
+        return num
+    }
+    
+    var first = 0
+    var second = 1
+    for _ in 1..<num {
+        let sum = first + second
+        first = second
+        second = sum
+    }
+    return second
+}
+
+var funcs = [fib1, fib2];
+for aFunc in funcs {
+    // checkin
+    let start = NSDate()
+    
+    for item in 0...25 {
+//        print("\(aFunc(item)), ", separator: "", terminator: "")
+        aFunc(item)
+    }
+    
+    // checkout
+    let end = NSDate()
+    let diff = end.timeIntervalSince1970 - start.timeIntervalSince1970
+    
+    print("")
+    print("\(String(describing: aFunc)) dur: \(diff)\"")
+}
