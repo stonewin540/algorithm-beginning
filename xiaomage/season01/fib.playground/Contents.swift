902151aa7788e25d7cd423844a15372084f26d6c
sidun.wen <sidun.wen@linkingcities.com>
2021-09-12 11:52:57 +0800

    Regroup Fibonacci;

--- xiaomage/season01/fib.playground/Contents.swift
+++ xiaomage/season01/fib.playground/Contents.swift
@@ -34,15 +34,16 @@ for aFunc in funcs {
     // checkin
     let start = NSDate()
     
-    for item in 0...25 {
+//    for item in 0...25 {
 //        print("\(aFunc(item)), ", separator: "", terminator: "")
-        aFunc(item)
-    }
+        let item = 10
+        print("\(item)th. fib number is: \(aFunc(item))")
+//    }
     
     // checkout
     let end = NSDate()
     let diff = end.timeIntervalSince1970 - start.timeIntervalSince1970
     
     print("")
-    print("\(String(describing: aFunc)) dur: \(diff)\"")
+    print("\(String(describing: aFunc))\(type(of: aFunc)) dur: \(diff) sec(s).")
 }
